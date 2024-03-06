1f483acce5e35e5312d8f44dc860330764d307e5
Vibhuti Joshi <vp1373@srmist.edu.in>
2024-03-06 19:16:48 +0530

    commit

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,14 +1,14 @@
 import React,{useState}from 'react'
+
 export default function Textform(props) {
   const handleUpClick = ()=>{
     console.log("UpperCase was clicked" + text );
     let newText= text.toUpperCase();
     setText(newText)
-    setText("You have clicked on handleUpClick")
+    //setText("You have clicked on handleUpClick")
   }
   const handleOnChange = (event)=>{
-    console.log("On Change");
-    setText(event.target.value);
+    setText(event.target.value)
   }
   const [text, setText] = useState('Enter text here'); //created a state which is text whose value is enter text here //text is a state variable .it can be updated using setText function.
   return (
