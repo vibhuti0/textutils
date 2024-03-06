5b539968beaad4e8d010f5e3b34da417d2f96465
Vibhuti Joshi <vp1373@srmist.edu.in>
2024-03-06 21:24:22 +0530

    commit

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -7,6 +7,18 @@ export default function Textform(props) {
     setText(newText)
     //setText("You have clicked on handleUpClick")
   }
+  const handleLoClick = ()=>{
+    console.log("UpperCase was clicked" + text );
+    let newText= text.toLowerCase();
+    setText(newText)
+    //setText("You have clicked on handleUpClick")
+  }
+  const handleClearClick = ()=>{
+    console.log("UpperCase was clicked" + text );
+    let newText= " ";
+    setText(newText)
+    //setText("You have clicked on handleUpClick")
+  }
   const handleOnChange = (event)=>{
     setText(event.target.value)
   }
@@ -20,10 +32,15 @@ export default function Textform(props) {
   <textarea className="form-control"  value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea> 
 </div>
 <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button> 
+<button className="btn btn-primary mx-1" onClick={handleLoClick}>Lowercase</button>
+<button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
     </div>
     <div className="container my-2">
       <h1>Your text summary</h1>
       <p>{text.split(" ").length} words and {text.length}</p>
+      <p>{0.008 * text.length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
     </div>
     </>
   )
