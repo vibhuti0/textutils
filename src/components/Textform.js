69b7627f66bdbfda37f98089c15ccb6637e18eea
Vibhuti Joshi <vp1373@srmist.edu.in>
2024-03-06 19:00:46 +0530

    commit

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -4,18 +4,19 @@ export default function Textform(props) {
     console.log("UpperCase was clicked");
     setText("You have clicked on handleUpClick")
   }
-  const handleOnChange = ()=>{
+  const handleOnChange = (event)=>{
     console.log("On Change");
+    setText(event.target.value);
   }
-  const [text, setText] = useState('Enter text here'); //created a state which is text whose value is enter text here
+  const [text, setText] = useState('Enter text here'); //created a state which is text whose value is enter text here //text is a state variable .it can be updated using setText function.
   return (
     <div>
      <h1>{props.heading} - {text}</h1>
 <div className="mb-3">
   
-  <textarea className="form-control"  value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+  <textarea className="form-control"  value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea> 
 </div>
-<button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+<button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button> 
     </div>
   )
 }
