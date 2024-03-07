3a10e31110f975dc7dd70d5c33f90c1563d24db5
Vibhuti Joshi <vp1373@srmist.edu.in>
2024-03-07 19:07:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+//import Textform from './components/Textform';
 function App() {
   return (
     <>
     <Navbar title="TextUtils"/>
     <div className="container my-3">
-    <Textform heading="Enter the text to analyze"/>
+    {/*<Textform heading="Enter the text to analyze"/>*/}
+    <About/>
     </div>
     </>
   );
