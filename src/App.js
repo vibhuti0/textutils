e01d34321377ba2abc9064b51de4e365d45d17c7
Vibhuti Joshi <vp1373@srmist.edu.in>
2024-03-09 14:13:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 
+//import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
-//import Textform from './components/Textform';
+import Textform from './components/Textform';
+import React,{useState} from 'react';
 function App() {
+  const[mode,setMode]=useState('light');// whether dark mode is enabled or not
   return (
     <>
-    <Navbar title="TextUtils"/>
+    <Navbar title="TextUtils" mode={mode}/>
     <div className="container my-3">
-    {/*<Textform heading="Enter the text to analyze"/>*/}
-    <About/>
+      <Textform heading="Enter the text to analyze"/>
     </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
-export default function Navbar() {
+import PropTypes from 'prop-types'
+export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-dark`}>
   <div className="container-fluid">
     <a className="navbar-brand" href="/">TextUtils</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,10 +18,10 @@ export default function Navbar() {
         </li>
        
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault"/>
+  <label class="form-check-label" for="flexSwitchCheckDefault">Enable Dark Mode</label>
+</div>
     </div>
   </div>
 </nav>
