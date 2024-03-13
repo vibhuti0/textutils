75d39fc78691e889bbf0f14d63b6c36a159d4f87
Vibhuti Joshi <vp1373@srmist.edu.in>
2024-03-13 22:25:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 //import { useState } from 'react';
 import './App.css';
-//import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import React,{useState} from 'react';
-import Alert from './components/Alert'
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   const[mode,setMode]=useState('light');// whether dark mode is enabled or not
   const[alert,setAlert]=useState(null);
@@ -40,7 +46,9 @@ function App() {
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3">
+      
       <Textform showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
+      <About/>
     </div>
     </>
   );
